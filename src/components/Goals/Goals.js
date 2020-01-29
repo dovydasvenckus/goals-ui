4ba50fe7bda7763c15d5cddf848e38c509767d3f
Dovydas Venckus <dovydas.venck@gmail.com>
2020-01-29 21:49:08 +0200

    Add react and other necessary libs

---
+++ src/components/Goals/Goals.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "bulma";
+
+const Goals = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Goals:</h1>
+    </div>
+  );
+};
+
+export default Goals;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Goals from "./components/Goals/Goals";
+
+ReactDOM.render(<Goals />, document.getElementById("appContainer"));
