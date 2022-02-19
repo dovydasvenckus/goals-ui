3b8450ab2c03e75a307dcc74a7a212f0b011c922
Dovydas Venckus <dovydas.venckus@live.com>
2022-02-19 12:55:02 +0200

    Color code progress bar depending on completion

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,12 +3,29 @@ import PropTypes from "prop-types";
 
 const ProgressBar = ({ completedPercentage }) => (
   <progress
-    className="progress is-info is-large"
+    className={`progress is-large ${getColorClass(completedPercentage)}`}
+    style={{progressBarBackgroundColor: "$green"}}
     value={completedPercentage}
     max="100"
   >
     {`${completedPercentage}%`}
   </progress>
 );
+
+const getColorClass = (percentage) => {
+  if (!percentage || percentage < 25) {
+    return 'is-danger'
+  }
+
+  if (percentage < 50) {
+    return 'is-warning'
+  }
+
+  if (percentage < 100) {
+    return 'is-primary'
+  }
+
+  return 'is-success'
+} 
 
 export default ProgressBar;
